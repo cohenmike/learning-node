055f530b9c2b4f0f832e8c2808f17349a948e1f4
Michael Cohen <Michael Cohen>
2017-01-31 23:12:50 -0600

    async fun

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 let fs = require('fs');
 
-fs.readFile("README2.md", function(err, data){
+fs.readFile("README.md", function(err, data){
     if (err != null){
         console.log(err.toString()); 
         return;
@@ -9,3 +9,20 @@ fs.readFile("README2.md", function(err, data){
 
     console.log(data.toString());
 });
+
+let rogueOne = function(foo, callback){
+    process.nextTick(function(){console.log(foo);});
+    process.nextTick(callback);
+}
+
+console.log("Starting...");
+
+rogueOne("R2D2", function(){
+    console.log("Found R2");
+});
+
+rogueOne("C3P0", function(){
+    console.log("Found 3P0")
+});
+
+console.log("End...");
