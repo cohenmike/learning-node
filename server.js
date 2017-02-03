8a74fe8529b4d13950c47a91ec23c50bf99a8664
Michael Cohen <Michael Cohen>
2017-02-02 22:30:27 -0600

    simple chat

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var app = require("express")();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-io.on('connection', function(client){
-    console.log('Client connected...');
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
 });
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(8080, function(){
-    console.log('listening on port 8080');
-});
+server.listen(8080);
